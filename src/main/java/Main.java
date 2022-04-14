67d4b4c613b378bd5d554121b65bf42e1deb373a
jlmoody3 <jlmoody3@asu.edu>
2022-04-14 11:04:03 -0700

    Checkstyle corrections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
 package main.java;
 
+/**
+ * Class that prints out "Hello world!"
+ * @author jessm
+ * @version 1.0
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+    /**
+     * This main method prints out "Hello world!"
+     * @param args - no arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-	}
+    }
 
 }
